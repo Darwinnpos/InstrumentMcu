7194e20ab45566bca63d463a1c746b84a07626e8
licheng26032 <licheng_tj@126.com>
2022-06-05 04:45:10 +0800

    add basic framework

---
+++ InstrumentMcu/applications/include/pin_config.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2006-2021, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2022-06-05     Administrator       the first version
+ */
+#ifndef APPLICATIONS_INCLUDE_PIN_CONFIG_H_
+#define APPLICATIONS_INCLUDE_PIN_CONFIG_H_
+
+#define PIN_BOARD_TYPE_BIT0 23
+
+
+
+
+
+
+
+
+
+#endif /* APPLICATIONS_INCLUDE_PIN_CONFIG_H_ */
